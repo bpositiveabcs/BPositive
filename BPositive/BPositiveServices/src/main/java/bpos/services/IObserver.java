a2acdd6a37d695df351d82486022442b410e7fad
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-01 16:41:34 +0300

    Revert "login/logout"
    This reverts commit d27d7bbe3d09b9fd0d609b281eb6a333bdda86b2.

--- BPositive/BPositiveServices/src/main/java/bpos/services/IObserver.java
+++ BPositive/BPositiveServices/src/main/java/bpos/services/IObserver.java
@@ -1,11 +1,5 @@
 package bpos.services;
 
-import bpos.model.Donation;
-import bpos.model.Event;
-
-import java.rmi.ServerException;
-
 public interface IObserver {
-    void eventHappened(Event event) throws ServicesExceptions;
-    void donationRegistered(Donation donation) throws ServicesExceptions;
+
 }
